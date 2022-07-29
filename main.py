921518b192a450fa9b80d90e0b98d18381ea3469
manins1618 <99032446+manins1618@users.noreply.github.com>
2022-07-29 22:40:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ def predict():
 
 
 # last line of code
-app.run() # debug = True let us update this code on live server. 
+app.run(debug=True) # debug = True let us update this code on live server. 
 
 
 
